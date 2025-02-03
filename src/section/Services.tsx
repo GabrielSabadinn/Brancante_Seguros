6e3bdcf379828e9cdff660a47342bcaa577fa5f8
Gabriel Sabadin <gbsabadin@icloud.com>
2025-02-03 09:46:28 -0300

    bug fix

--- src/section/Services.tsx
+++ src/section/Services.tsx
@@ -1,27 +1,28 @@
 import { FaFeather } from "react-icons/fa";
 import { productItens } from "../constants/productItens";
-import "../styles/ServicesStyle.css";
+import "../styles/securityStyle.css";
 import products from "../assets/screens/products.png";
 
 export function ServicesSection() {
-    return (
-        <div className="container">
-            <img src={products} alt="Produtos" className="styled-image" />
-            <h2 className="screen-title">Principais Serviços e Benefícios</h2>
-            <p className="screen-text">
-                Descubra como podemos proteger seu patrimônio náutico com as soluções certas para você:
-            </p>
-            <div className="list-container">
-                {productItens.map((product) => (
-                    <div key={product.id} className="list-item-container">
-                        <div className="list-item">
-                            <FaFeather name="check" size={24} color="green" />
-                            <span className="list-text">{product.title}</span>
-                        </div>
-                        <p className="list-description">{product.description}</p>
-                    </div>
-                ))}
+  return (
+    <div className="container">
+      <img src={products} alt="Produtos" className="styled-image" />
+      <h2 className="screen-title">Principais Serviços e Benefícios</h2>
+      <p className="screen-text">
+        Descubra como podemos proteger seu patrimônio náutico com as soluções
+        certas para você:
+      </p>
+      <div className="list-container">
+        {productItens.map((product) => (
+          <div key={product.id} className="list-item-container">
+            <div className="list-item">
+              <FaFeather name="check" size={24} color="green" />
+              <span className="list-text">{product.title}</span>
             </div>
-        </div>
-    );
+            <p className="list-description">{product.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
