0575c648e5f5096c23d1c1097641519518d2e598
Gabriel Sabadin <gbsabadin@icloud.com>
2025-02-06 13:24:45 -0300

    update

--- src/components/dowloadPdf.tsx
+++ src/components/dowloadPdf.tsx
@@ -1,15 +1,16 @@
 import "../styles/readingBtnStyle.css";
 
 const DownloadPdf = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/sabores-a-bordo.pdf";
+    link.download = "sabores-a-bordo.pdf";
+    link.click();
+  };
+
   return (
-    <button className="button-style">
-      <a
-        href="/sabores-a-bordo.pdf"
-        download="sabores-a-bordo.pdf"
-        className="reading-btn-text"
-      >
-        Baixar PDF
-      </a>
+    <button className="button-style" onClick={handleDownload}>
+      <p className="reading-btn-text">Baixar PDF</p>
     </button>
   );
 };
